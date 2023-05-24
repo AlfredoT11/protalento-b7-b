30b330bda8dff6a06bbd3a81ec692c929fc91a04
Tonatiuh Díaz <alfredo117tdg@hotmail.com>
2023-05-23 20:12:26 -0600

    Intro a JS

---
+++ 2.1.1_intro_js/script.js
@@ -0,0 +1,14 @@
+/* Este es un script para aprender
+:D
+*/
+
+//alert("¡Hola Mundo 2!");
+
+console.log("Hola desde la consola");
+document.write("<h2>Hola desde la página</h2>");
+
+var nombre = prompt("Escribe tu nombre: ");
+
+//document.write(nombre);
+document.write("<h2>¡Bienvenido " + nombre + "! :)</h2>");
+
