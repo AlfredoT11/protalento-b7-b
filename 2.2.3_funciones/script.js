e0848478c1b9292dd2bf6821681a6ce6359aa610
Tonatiuh Díaz <alfredo117tdg@hotmail.com>
2023-06-01 18:52:36 -0600

    Funciones globales

--- 2.2.3_funciones/script.js
+++ 2.2.3_funciones/script.js
@@ -2,7 +2,7 @@
 
 function sumar(num1, num2){
     let resultado = num1 + num2;
-    //return resultado;
+    return resultado;
 }
 
 let num1 = 4;
@@ -44,5 +44,26 @@ function funcionPadre2(){
 
 let resultadoDeFuncionHija = funcionPadre2();
 console.log(typeof resultadoDeFuncionHija);
-console.log(`El valor retornado es: ${resultadoDeFuncionHija}`)
+console.log(`El valor retornado es: ${resultadoDeFuncionHija}`);
 
+
+let resultado = 10;
+
+function restar(num){
+    console.log(`Resultado antes de operación: ${resultado}`);
+    resultado -= num; // resultado = resultado - num;
+    console.log(`Resultado después de operación: ${resultado}`);
+    return resultado;
+}
+
+function multiplicar(num){
+    console.log(`Resultado antes de operación: ${resultado}`);
+    resultado *= num;
+    console.log(`Resultado después de operación: ${resultado}`);
+    return resultado;
+}
+
+restar(5);
+multiplicar(2.5);
+
+console.log(`El resultado final es: ${resultado}`);
