71c6d8caa0d5ffb4a4d4d38d72426726771885a0
Tonatiuh Díaz <alfredo117tdg@hotmail.com>
2023-06-15 19:50:56 -0600

    Modificación de atributos en método comer

--- 2.4.2_IntroAPOO/script.js
+++ 2.4.2_IntroAPOO/script.js
@@ -1,16 +1,28 @@
+class Comportamiento{
+    constructor(comportamientoAnimales, comportamientoPersonas){
+        this.comportamientoAnimales = comportamientoAnimales;
+        this.comportamientoPersonas = comportamientoPersonas;
+    }
+}
+
+
 class Animal{
 
-    constructor(raza, numPatas, color, esSalvaje, tipoDieta, sonido){
+    constructor(raza, numPatas, color, esSalvaje, tipoDieta, sonido, comportamientoAnimales, comportamientoPersonas, peso){
         this.raza = raza;
         this.numPatas = numPatas;
         this.color = color;
         this.esSalvaje = esSalvaje;
         this.tipoDieta = tipoDieta;
         this.sonido = sonido;
+        this.comportamiento = new Comportamiento(comportamientoAnimales, comportamientoPersonas);
+        this.peso = peso;
     }
 
     comer(kilosDeComida){
         console.log(`Soy un ${this.raza} y voy a comer ${kilosDeComida} kg de comida`);
+        this.peso += kilosDeComida;
+        return this.peso;
     }
 
     comunicarse(){
@@ -27,12 +39,17 @@ class Animal{
 
 }
 
-let perro = new Animal("Perro", 4, "Cafe", false, "Carnívoro", "woooof");
+let perro = new Animal("Perro", 4, "Cafe", false, "Carnívoro", "woooof", "miedo", "amistoso", 20);
 
-perro.comer(1);
+console.log(`Este ${perro.raza} pesa ${perro.peso}`);
+let nuevoPeso = perro.comer(1);
+console.log(`Despues de comer pesa: ${perro.peso}`);
+
 perro.comunicarse();
 
 
 console.log(`Mi animal es: ${perro.tipoDieta}`);
 perro.tipoDieta = 'Hervíboro';
 console.log(`Mi animal es: ${perro.tipoDieta}`);
+
+console.log(`Mi animal ${perro.raza} es ${perro.comportamiento.comportamientoPersonas} con las personas`);
