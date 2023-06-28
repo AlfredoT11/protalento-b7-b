766288028780d356dcc84726ca35f5e9a38652b9
Malinali Becerril <mali@devf.mx>
2023-06-27 20:40:18 -0600

    agregar la exportación de la función

--- 3.1.3_Modules/c/b/a.js
+++ 3.1.3_Modules/c/b/a.js
@@ -1,8 +1,14 @@
-// module.exports.edad = edad;
+// archivo trae todo lo que hay en module.exports
 const archivo = require("../../index");
+// module.exports.edad = edad;
+const { edad } = require("../../index");
 
-console.log("Holi ->", archivo);
+const { saludarEnConsola } = require("../../index");
+
+console.log("Holi ->", archivo, edad);
 
 archivo.saludarEnConsola("Mali");
+
+console.log(saludarEnConsola);
 
 
