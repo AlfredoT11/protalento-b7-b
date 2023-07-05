86815d15a83a6b796a9c7b7cf1e9cc0b569fc2aa
Tonatiuh Díaz <alfredo117tdg@hotmail.com>
2023-07-04 20:15:43 -0600

    Métodos asincrónicos

--- 3.3.1_Promesas/script.js
+++ 3.3.1_Promesas/script.js
@@ -43,14 +43,18 @@ promesa.then((valorPromesaCumplida) => {
 });
 */
 
+/*
 let promesaAnidada1 = new Promise((resolve, reject) => {
     setTimeout(() => {
         resolve("Promesa cumplida. :D");
     }, 1000);
 });
 
+// Promesas anidadas
 promesaAnidada1.then((value) => {
     console.log("Promesa anidada 1 cumplida");
+    // Si quieres anidar promesas, nosotros debemos retornar una promesa
+    // dentro del then.
     return new Promise((resolve, reject) => {
         setTimeout(() => {
             console.log("Cronometro de promesa anidada");
@@ -69,6 +73,68 @@ promesaAnidada1.then((value) => {
 }).then((value) => {
     console.log(value);
 })
+// El código del callback del catch se ejecutará en caso de que CUALQUIER
+// promesa anidada no se cumpla.
 .catch(() => {
     console.log("Ocurrió un error con alguna promesa");
-})
+});
+*/
+
+let promesa1s = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        console.log("Promesa 1s");
+        resolve("Promesa 1s");
+    }, 1000);
+});
+
+let promesa2s = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        console.log("Promesa 2s");
+        resolve("Promesa 2s");
+    }, 2000);
+});
+
+let promesa5s = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        console.log("Promesa 5s");
+        resolve("Promesa 5s");
+    }, 5000);
+});
+
+// Este método únicamente va a ejecutar el código en then
+// cuando TODAS las promesas se cumplan.
+// En caso de que UNA SOLA promesa no se cumpla, se ejecuta el callback del catch.
+
+/*
+Promise.all([promesa1s, promesa2s, promesa5s]).then(([valorPromesa1, valorPromesa2, valorPromesa3]) => {
+    console.log("Las 3 promesas se han cumplido");
+}).catch((error) => {
+    console.log("Alguna de las promesas falló");
+});
+
+// Este método ejecuta el callback del then cuando TODAS las promesas
+// terminen sin importar que se cumplan o no.
+Promise.allSettled([promesa1s, promesa2s, promesa5s]).then(([valorPromesa1, valorPromesa2, valorPromesa3]) => {
+    console.log("Las 3 promesas se han terminado");
+}).catch((error) => {
+    console.log("Alguna de las promesas falló");
+});
+*/
+
+// Este método ejecuta el callback del then cuando UNA PROMESA se cumple.
+// En caso de que NINGUNA promesa se cumpla, se ejecutará el callbak del catch.
+Promise.any([promesa1s, promesa2s, promesa5s]).then((valorPrimerPromesa) => {
+    console.log("Se ha cumplido una promesa");
+    console.log(valorPrimerPromesa);
+}).catch(() => {
+    console.log("Ninguna promesa se cumplió. );");
+});
+
+// Este método ejecuta el callback del then cuando UNA PROMESA termina.
+Promise.race([promesa1s, promesa2s, promesa5s]).then((valorPrimerPromesa) => {
+    console.log("Se ha terminado una promesa");
+    console.log(valorPrimerPromesa);
+}).catch(() => {
+    console.log("Ninguna promesa se cumplió. );");
+});
+
