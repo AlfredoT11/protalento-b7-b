a6dc80cf5d2fd3e07a77778ed09b803806e356e0
Tonatiuh Díaz <alfredo117tdg@hotmail.com>
2023-07-27 20:31:28 -0600

    Creación de post

--- 4.2.3_IntroABackend/script.js
+++ 4.2.3_IntroABackend/script.js
@@ -2,9 +2,18 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
     res.send('¡Hola mundo!');
 });
+
+app.post('/', (req, res) => {
+    const body = req.body;
+    body.mensaje = 'Petición atendida';
+    res.send(body);
+});
+
 
 app.listen(port, () => {
     console.log(`Aplicación escuchando en el puerto ${port}`);
