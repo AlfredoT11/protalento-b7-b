ad7d945571b4f0e9cf5d6beef863db3bd4ea77eb
Malinali Becerril <mali@devf.mx>
2023-08-31 20:46:07 -0600

    add conection mongoose

--- 5.1.1_no_sql/index.js
+++ 5.1.1_no_sql/index.js
@@ -1,12 +1,24 @@
 const express = require('express');
-
+const mongoose = require('mongoose');
 const server = express();
 
 const PORT = 3000;
 
 const routes = require('./routes');
+const URI_MONGO = 'mongodb+srv://malinalibb98:<password>@cluster0.tsrujhv.mongodb.net/?retryWrites=true&w=majority';
 
 server.use('/api/v1', routes);
+
+const mongoConnect = async () => {
+    try {
+        await mongoose.connect(URI_MONGO);
+        console.log('Conectamos a la base de datos.');
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+mongoConnect();
 
 server.listen(PORT, () => {
     console.log(`Funcionando en puerto ${PORT}`);
