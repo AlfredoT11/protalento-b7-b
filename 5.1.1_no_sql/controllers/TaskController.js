56145eef00a32f0e5d68103d9152e4c1dcb8a980
Malinali Becerril <mali@devf.mx>
2023-09-05 18:38:55 -0600

    task routes and controllers

--- 5.1.1_no_sql/controllers/TaskController.js
+++ 5.1.1_no_sql/controllers/TaskController.js
@@ -1,9 +1,27 @@
 
-const getAll = (req, res) => {
-    // código para conectar
-    return res.send('todas mis tareas');
-}
-
 module.exports = {
-    getAll,
+    getAllTasks: (req, res) => {
+        // código para conectar
+        return res.send('getAllTasks');
+    },
+    getOneTaskById: (req, res) => {
+        // código para conectar
+        return res.send('getOneTaskById');
+    },
+    searchTasks: (req, res) => {
+        // código para conectar
+        return res.send('searchTasks');
+    },
+    addTask: (req, res) => {
+        // código para conectar
+        return res.send('addTask');
+    },
+    updateTask: (req, res) => {
+        // código para conectar
+        return res.send('updateTask');
+    },
+    deleteTask: (req, res) => {
+        // código para conectar
+        return res.send('deleteTask');
+    },
 };
--- 5.1.1_no_sql/routes/TaskRoutes.js
+++ 5.1.1_no_sql/routes/TaskRoutes.js
@@ -2,8 +2,35 @@ const express = require('express');
 
 const router = express.Router();
 
-const TaskController = require('../controllers/TaskController');
+const {
+    getAllTasks,
+    getOneTaskById,
+    searchTasks,
+    addTask,
+    updateTask,
+    deleteTask,
+} = require('../controllers/TaskController');
 
-router.get('/', TaskController.getAll);
+
+// Obtener todas las tareas
+router.get('/', getAllTasks);
+
+// Obtener una tarea
+router.get('/:id', getOneTaskById);
+// Buscar tareas
+router.get('/search', searchTasks);
+
+//Agregar una tarea  createTask
+router.post('/', addTask);
+
+// Actualizar todo el objeto put ~~
+// Actualizar una propiedad del objeto
+router.patch('/:id', updateTask);
+
+//Eliminar una tarea
+
+router.delete('/:id', deleteTask);
+
+
 
 module.exports = router;
