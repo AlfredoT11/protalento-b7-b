ce846b233c64ddfa529aaf89b6fcc64c90330ec7
Tonatiuh Díaz <alfredo117tdg@hotmail.com>
2023-09-13 08:07:53 -0600

    Standard

--- 5.1.1_no_sql/index.js
+++ 5.1.1_no_sql/index.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const server = express();
 
 
-const PORT = 3000;
+const PORT = process.env.PORT;
 
 const routes = require('./routes');
 const URI_MONGO = process.env.MONGO_URI;
